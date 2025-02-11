99dabc02a8c58297e2ba0626f0d5c178e36190e5
Kanishka2301 <kanishkaskumar2004@gmail.com>
2025-02-11 14:30:23 +0530

    layout commit

--- components/header.jsx
+++ components/header.jsx
@@ -2,6 +2,8 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import Image from "next/image";
 import React from "react";
+import { Button } from "./ui/button";
+import { LayoutDashboard } from "lucide-react";
 
 const Header = () => {
   return (
@@ -16,6 +18,16 @@ const Header = () => {
             className="h-12 py-1 w-auto object-contain"
           />
         </Link>
+        <div>
+          <SignedIn>
+            <Link href={"/dashboard"}>
+              <Button>
+                <LayoutDashboard className="h-4 w-4" />
+                Industry Insights
+              </Button>
+            </Link>
+          </SignedIn>
+        </div>
       </nav>
       <SignedOut>
         <SignInButton />
