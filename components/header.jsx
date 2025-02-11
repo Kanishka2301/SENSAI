a430f971b585f23fcb1bc09fb827e71a10ff69a1
Kanishka2301 <kanishkaskumar2004@gmail.com>
2025-02-11 15:25:02 +0530

    insights commit

--- components/header.jsx
+++ components/header.jsx
@@ -3,7 +3,20 @@ import Link from "next/link";
 import Image from "next/image";
 import React from "react";
 import { Button } from "./ui/button";
-import { LayoutDashboard } from "lucide-react";
+import {
+  ChevronDown,
+  FileText,
+  GraduationCap,
+  LayoutDashboard,
+  PenBox,
+  StarsIcon,
+} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Header = () => {
   return (
@@ -21,7 +34,7 @@ const Header = () => {
             className="h-12 py-1 w-auto object-contain"
           />
         </Link>
-        <div>
+        <div className="flex items-center space-x-2 md:space-x-4">
           <SignedIn>
             <Link href={"/dashboard"}>
               <Button>
@@ -30,6 +43,35 @@ const Header = () => {
               </Button>
             </Link>
           </SignedIn>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Button>
+                <StarsIcon className="h-4 w-4" />
+                <span className="hidden md:block">Growth Tools</span>
+                <ChevronDown className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>
+                <Link href={"/resume"} className="flex items-center gap-2">
+                  <FileText className="h-4 w-4" />
+                  <span>Build Resume</span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href={"/resume"} className="flex items-center gap-2">
+                  <PenBox className="h-4 w-4" />
+                  <span>Cover Letter</span>
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href={"/resume"} className="flex items-center gap-2">
+                  <GraduationCap className="h-4 w-4" />
+                  <span>Interview Prep</span>
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </nav>
       <SignedOut>
