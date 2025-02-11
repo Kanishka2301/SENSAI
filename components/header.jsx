cf044a57ed71a4bab7c73e01035aed378cd48932
Kanishka2301 <kanishkaskumar2004@gmail.com>
2025-02-11 14:26:13 +0530

    image commit

--- components/header.jsx
+++ components/header.jsx
@@ -1,16 +1,29 @@
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
 const Header = () => {
   return (
-    <div>
+    <header>
+      <nav>
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="Sensai Logo"
+            width={200}
+            height={60}
+            className="h-12 py-1 w-auto object-contain"
+          />
+        </Link>
+      </nav>
       <SignedOut>
         <SignInButton />
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
-    </div>
+    </header>
   );
 };
 
