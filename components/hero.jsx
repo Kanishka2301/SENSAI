abb2f0bc423b878e294758b2c27609aeef55722f
Kanishka2301 <kanishkaskumar2004@gmail.com>
2025-02-12 19:59:19 +0530

    landingpage commit

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
+import Image from "next/image";
 
 const HeroSection = () => {
   return (
     <section className="w-full pt-36 md:pt-48 pb-10">
-      <div>
-        <div>
-          <h1>
+      <div className="space-y-6 text-center">
+        <div className="space-y-6 mx-auto">
+          <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title">
             Your AI Career Coach for
             <br />
             Professional Success
@@ -28,6 +29,18 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
+        </div>
+        <div>
+          <div>
+            <Image
+              src={"/banner.jpeg"}
+              width={1280}
+              height={720}
+              alt="Sensai Banner"
+              className="rounded-lg shadow-2xl border mx-auto"
+              priority
+            />
+          </div>
         </div>
       </div>
     </section>
