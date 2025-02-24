be0a83f0a7519bd0868537dc9fc69db0b948c67e
Kanishka2301 <kanishkaskumar2004@gmail.com>
2025-02-24 21:03:43 +0530

    api commit

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,8 +1,13 @@
 import { industries } from "@/data/industries";
 import { getUserOnboardingStatus } from "@/actions/user";
+import { redirect } from "next/navigation";
+import OnboardingForm from "./_components/onboarding-form";
 
 const OnboardingPage = async () => {
   const { isOnboarded } = await getUserOnboardingStatus();
+  if (isOnboarded) {
+    redirect("/dashboard");
+  }
   return (
     <main>
       <OnboardingForm industries={industries} />
