f10435188acf1eec67a6113d978d25effdd411c3
Kanishka2301 <kanishkaskumar2004@gmail.com>
2025-02-25 11:04:57 +0530

    userOnboarding commit

--- app/(main)/onboarding/_components/onboarding-form.jsx
+++ app/(main)/onboarding/_components/onboarding-form.jsx
@@ -1,5 +1,43 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { onboardingSchema } from "@/app/lib/schema";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 const OnboardingForm = ({ industries }) => {
-  return <div>OnboardingForm</div>;
+  const [selectedIndustry, setSelectedIndustry] = useState(null);
+  const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    watch,
+  } = useForm({
+    resolver: zodResolver(onboardingSchema),
+  });
+  return (
+    <div className="flex items-center justify-center bg-background">
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default OnboardingForm;
