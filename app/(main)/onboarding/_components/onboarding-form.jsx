4b46f5eb4735790c6cf1a876174ddb2a579657c9
Kanishka2301 <kanishkaskumar2004@gmail.com>
2025-02-25 11:12:23 +0530

    userOnboarding commit

--- app/(main)/onboarding/_components/onboarding-form.jsx
+++ app/(main)/onboarding/_components/onboarding-form.jsx
@@ -12,6 +12,14 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Label } from "@/components/ui/label";
 
 const OnboardingForm = ({ industries }) => {
   const [selectedIndustry, setSelectedIndustry] = useState(null);
@@ -27,13 +35,36 @@ const OnboardingForm = ({ industries }) => {
   });
   return (
     <div className="flex items-center justify-center bg-background">
-      <Card>
+      <Card className="w-full max-w-lg mt-10 mx-2">
         <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
+          <CardTitle className="gradient-title text-4xl">
+            Complete Your Profile
+          </CardTitle>
+          <CardDescription>
+            Select your industry to get personalized career insights and
+            recommendations.
+          </CardDescription>
         </CardHeader>
         <CardContent>
-          <p>Card Content</p>
+          <form>
+            <div>
+              <Label htmlFor="industry">Industry</Label>
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Theme" />
+                </SelectTrigger>
+                <SelectContent>
+                  {industries.map((ind) => {
+                    return (
+                      <SelectItem value={ind.id} key={ind.id}>
+                        {ind.name}
+                      </SelectItem>
+                    );
+                  })}
+                </SelectContent>
+              </Select>
+            </div>
+          </form>
         </CardContent>
       </Card>
     </div>
