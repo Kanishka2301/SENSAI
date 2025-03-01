1e84c540b8ada43ccd9d28e5ac8cfb695adeff87
Kanishka2301 <kanishkaskumar2004@gmail.com>
2025-03-01 11:19:00 +0530

    Insights commit

--- app/(main)/dashboard/_components/dashboard-view.jsx
+++ app/(main)/dashboard/_components/dashboard-view.jsx
@@ -1,8 +1,64 @@
 "use client";
+import {
+  BriefcaseIcon,
+  LineChart,
+  TrendingUp,
+  TrendingDown,
+  Brain,
+} from "lucide-react";
 import React from "react";
+import { format, formatDistanceToNow } from "date-fns";
+import { Badge } from "@/components/ui/badge";
 
 const DashboardView = ({ insights }) => {
-  return <div>DashboardView</div>;
+  const salaryData = insights.salaryRanges.map((range) => ({
+    name: range.role,
+    min: range.min / 1000,
+    max: range.max / 1000,
+    median: range.median / 1000,
+  }));
+
+  const getDemandLevelColor = (level) => {
+    switch (level.toLowerCase()) {
+      case "high":
+        return "bg-green-500";
+      case "medium":
+        return "bg-yellow-500";
+      case "low":
+        return "bg-red-500";
+      default:
+        return "bg-gray-500";
+    }
+  };
+  const getMarketOutlookInfo = (outlook) => {
+    switch (outlook.toLowerCase()) {
+      case "positive":
+        return { icon: TrendingUp, color: "text-green-500" };
+      case "neutral":
+        return { icon: LineChart, color: "text-yellow-500" };
+      case "negative":
+        return { icon: TrendingDown, color: "text-red-500" };
+      default:
+        return { icon: LineChart, color: "text-gray-500" };
+    }
+  };
+
+  const OutlookIcon = getMarketOutlookInfo(insights.marketOutlook).icon;
+  const outlookColor = getMarketOutlookInfo(insights.marketOutlook).color;
+
+  const lastUpdatedDate = format(new Date(insights.lastUpdated), "dd/MM/yyyy");
+  const nextUpdateDistance = formatDistanceToNow(
+    new Date(insights.nextUpdate),
+    { addSuffix: true }
+  );
+
+  return (
+    <div className="space-y-6">
+      <div className="flex justify-between items-center">
+        <Badge variant="outline">Last updated:{lastUpdatedDate}</Badge>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardView;
