01b375fb6261d8104cb19e8d30993bb612380e04
Kanishka2301 <kanishkaskumar2004@gmail.com>
2025-03-05 14:24:53 +0530

    resume commit

---
+++ actions/resume.js
@@ -0,0 +1,13 @@
+import { auth } from "@clerk/nextjs/server";
+import { db } from "@/lib/prisma";
+
+export async function saveResume(content) {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) throw new Error("User not found");
+}
