9bea3bee10e5e81eedc01963a1497256dce2c9fc
Kanishka2301 <kanishkaskumar2004@gmail.com>
2025-03-05 14:34:49 +0530

    resume commit

--- actions/resume.js
+++ actions/resume.js
@@ -1,5 +1,6 @@
 import { auth } from "@clerk/nextjs/server";
 import { db } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
 
 export async function saveResume(content) {
   const { userId } = await auth();
@@ -10,4 +11,41 @@ export async function saveResume(content) {
   });
 
   if (!user) throw new Error("User not found");
+
+  try {
+    const resume = await db.resume.upsert({
+      where: {
+        userId: user.id,
+      },
+      update: {
+        content,
+      },
+      create: {
+        userId: user.id,
+        content,
+      },
+    });
+    revalidatePath("/resume");
+    return resume;
+  } catch (error) {
+    console.error("Error saving resume:", error.message);
+    throw new Error("Failed to save the resume");
+  }
+}
+
+export async function getResume() {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) throw new Error("User not found");
+
+  return await db.resume.findUnique({
+    where: {
+      userId: user.id,
+    },
+  });
 }
