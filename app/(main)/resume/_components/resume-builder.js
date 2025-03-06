41d0bea0763d53cabc9e12e233c3de5616f27cf2
Kanishka2301 <kanishkaskumar2004@gmail.com>
2025-03-06 14:36:03 +0530

    resume commit

--- app/(main)/resume/_components/resume-builder.js
+++ app/(main)/resume/_components/resume-builder.js
@@ -1,5 +1,29 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Save } from "lucide-react";
+
 const ResumeBuilder = ({ initialContent }) => {
-  return <div>ResumeBuilder</div>;
+  return (
+    <div>
+      <div>
+        <h1 className="font-bold gradient-title text-5x md:text-6xl">
+          Resume Builder
+        </h1>
+
+        <div>
+          <Button variant="destructive">
+            <Save className="h-4 w-4" />
+            Save
+          </Button>
+          <Button variant="destructive">
+            <Save className="h-4 w-4" />
+            Download PDF
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ResumeBuilder;
